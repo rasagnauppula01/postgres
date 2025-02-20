6cf3c8cc20b4b1e8ca1c364d739a48332cd2feb3
Rasagna Uppula <147481746+rasagnauppula01@users.noreply.github.com>
2025-02-20 18:45:19 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const Users = sequelize.define("Users", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+module.exports = Users;
